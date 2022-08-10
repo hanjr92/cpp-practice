15b254d1625937d80ce01d28e4f0ee124305c7a1
hanjr92 <jiaronghan@outlook.com>
2022-08-10 20:47:47 +0800

    add mutable and const

--- kerwords/main.cpp
+++ kerwords/main.cpp
@@ -1,5 +1,22 @@
 #include<iostream>
 
+
+class Entity{
+
+    private:
+    mutable int x = 0; // mutable 表示该变量可以在const修饰的函数中被修改
+    int y = 0;
+    public:
+    void setX(int dx ) const
+    {
+        x = dx;
+    }
+    void setY(int dy ) const//声明成员函数时，末尾加const修饰，表示在成员函数内不得改变该对象的任何数据。这种模式常被用来表示对象数据只读的访问模式。
+    {
+        y = dy;
+    }
+};
+
 int main()
 {
     const int a = 8;
@@ -11,7 +28,15 @@ int main()
 
     int *const c = &temp;
     // c++;
-    (*c)++; // 
-    std::cout<< *b << std::endl;
+    (*c)++; // c是指针常量，指针指向的内存地址不可变，但指针指向的内存内数据可变
+    
+    int i = 10;
+    int j = 100;
+    const int &r = i;
+    int const &s = j;
+    // r = j; // 由于r是常量引用，不能对r进行修改
+    Entity e;
+    e.setX(10);
+    e.setY(10);
     std::cin.get();
 }
